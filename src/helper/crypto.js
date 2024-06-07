792b3c1fb23d659dd3549e50a28033994f56f437
farhannjb <farhan@koda.so>
2024-06-07 10:41:00 +0700

    update helper

--- src/helper/crypto.js
+++ src/helper/crypto.js
@@ -3,15 +3,11 @@ import { randomBytes, createHmac } from 'crypto'
 const SALTSIZE = 16
 
 const generateRandomSalt = (saltSize) => {
-  const salt = randomBytes(saltSize).toString('base64').slice(0, saltSize)
-
-  return salt
+  return randomBytes(saltSize).toString('base64').slice(0, saltSize)
 }
 
 const hashPassword = (pwd, salt) => {
-  const hash = createHmac('sha512', salt).update(pwd).digest('base64')
-
-  return hash
+  return createHmac('sha512', salt).update(pwd).digest('base64')
 }
 
 const match = (currPwd, hashedPwd, salt) => {
