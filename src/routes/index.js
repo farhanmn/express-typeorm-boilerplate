ce73171e3af09bb48e7e98d07fce5d04e411a419
Muhammad Farhan N <najibfarhan3@gmail.com>
2025-10-20 11:30:14 +0700

    bug fix reflect-metadata

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,10 @@ import users from './users.js'
 import sample from './sample.js'
 import { SC } from '../helper/statuscode.js'
 
-const errorHandler = (req, res) => {
-  res.stdJson(SC.NOT_FOUND, null, 'Invalid route')
-}
 export default (app) => {
-  app.use('/users', users), app.use('/sample', sample)
-  app.all('*', errorHandler)
+  app.use('/users', users)
+  app.use('/sample', sample)
+  app.use((req, res) => {
+    res.stdJson(SC.NOT_FOUND, null, 'Invalid route')
+  })
 }
